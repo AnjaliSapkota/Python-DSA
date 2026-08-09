f7e23243a9cb76e930d05630b57beab34a3d324a
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-08-09 14:07:05 +0545

    ft: created an empty linked list with 0 nodes

--- Linked_list/main.py
+++ Linked_list/main.py
@@ -9,3 +9,13 @@ print(a.data)
 print(a.next)
 b = Node(2)
 c = Node(3)
+
+class LinkedList:
+
+    def __init__(self):
+
+        # create an empty linked list
+        self.head = None
+        self.n = 0 # no of nodes in the linked list
+
+        
