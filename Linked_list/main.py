dca238da0fbdd5afa4e0b8a7433f7dcee2638c5e
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-08-09 23:18:56 +0545

    ft: create insertion at head, tail, middle and traversal

--- Linked_list/main.py
+++ Linked_list/main.py
@@ -18,4 +18,79 @@ class LinkedList:
         self.head = None
         self.n = 0 # no of nodes in the linked list
 
+    def __len__(self):
+        return self.n
+
+    def insert_head(self,value):
+
+        # new node
+        new_node = Node(value)
+
+        # create connection
+        new_node.next = self.head
+
+        #reassign head
+        self.head = new_node
+
+        self.n = self.n + 1 
+
+    def __str__(self):
+        curr = self.head
+
+        result = ''
+        while curr != None:
+            result = result + str(curr.data) + '->'
+            curr = curr.next
+
+        return result[:-2]
+
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head == None:
+            self.head == new_node
+            self.n = self.n + 1
+            return
         
+        curr = self.head
+
+        while curr.next != None:
+            curr = curr.next
+
+        # you are at the last node
+        curr.next = new_node
+        self.n = self.n + 1
+
+    def insert_after(self, after, value):
+        new_node = Node(value)
+
+        curr = self.head
+
+        while curr != None:
+            if curr.data == after:
+                break
+            curr = curr.next
+
+        if curr != None:
+            new_node.next = curr.next
+            curr.next = new_node
+        else:
+            return 'Item not found'
+        
+L = LinkedList() 
+
+# insert at head
+
+L.insert_head(1)
+print(len(L))
+L.insert_head(2)
+print(len(L))
+L.insert_head(3)
+print(len(L))
+L.insert_head(4)
+print(len(L))
+
+print(L)
+
+L.append(5)
+print(L)
