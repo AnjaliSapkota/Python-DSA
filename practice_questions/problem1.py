8b3acc786f6e8b2f5f0cfe0cb4f761662f956e31
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-08-24 21:26:42 +0545

    practice problem 1

--- practice_questions/problem1.py
+++ practice_questions/problem1.py
@@ -2,3 +2,19 @@
 # it by 1, and if a digit is 9 then replace it with 0.
 # For example: Input: 26319053
 # Output: 26420064
+
+num = input("Enter a number: ")
+
+result = ""
+
+for digit in num:
+    digit = int(digit)
+
+    if digit == 9:
+        result += "0"
+    elif digit % 2 != 0:
+        result += str(digit + 1)
+    else:
+        result += str(digit)
+
+print("Output:", result)
