f321285396eeaa424c800764fc543942d510947d
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-08-24 21:26:33 +0545

    practice problem 1

---
+++ practice_questions/problem1.py
@@ -0,0 +1,4 @@
+# Write a program to modify a number in such a way that if a digit is odd, increment
+# it by 1, and if a digit is 9 then replace it with 0.
+# For example: Input: 26319053
+# Output: 26420064
